9ed5a423b7e8b334f1e91c93d2900e5cab6d14d7
EvAlGodGood <Gooodfile@mail.ru>
2021-07-30 18:49:08 +0800

    HW 3. Выполнено все, не получилось поэкспериментировать над таблицей все съезжает, (вопрос не к теме, нужно с HTML лучше разбираться). Стало понятьнее как можно работать с сайтом (вопрос из прошлой работы).

---
+++ src/main/java/com/geekbrains/webapp/springwebapp/repositories/ProductsRepository.java
@@ -0,0 +1,37 @@
+package com.geekbrains.webapp.springwebapp.repositories;
+
+import com.geekbrains.webapp.springwebapp.model.Product;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ProductsRepository {
+    private List<Product> products;
+
+    @PostConstruct //подготовка списка продуктов при инициализации
+    public void init() {
+        this.products = new ArrayList<>(Arrays.asList(
+                new Product(1L, "Salt", 25),
+                new Product(2L, "Bread", 40),
+                new Product(3L, "Oil", 100),
+                new Product(4L, "Сoffee", 190)
+        ));
+    }
+//весь список
+    public List<Product> findAll() {
+        return Collections.unmodifiableList(products);
+    }
+//выдача по ID
+    public Product findById(Long id) {
+        return products.stream().filter(s -> s.getId().equals(id)).findFirst().get();
+    }
+//добавление продукта
+    public void save(Product product) {
+        products.add(product);
+    }
+}
---
+++ src/main/java/com/geekbrains/webapp/springwebapp/services/ProductsService.java
@@ -0,0 +1,30 @@
+package com.geekbrains.webapp.springwebapp.services;
+
+import com.geekbrains.webapp.springwebapp.model.Product;
+import com.geekbrains.webapp.springwebapp.repositories.ProductsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductsService {
+    private ProductsRepository productsRepository;
+
+    @Autowired
+    public ProductsService(ProductsRepository productsRepository) {
+        this.productsRepository = productsRepository;
+    }
+
+    public List<Product> findAll() {
+        return productsRepository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return productsRepository.findById(id);
+    }
+
+    public void save(Product product) {
+        productsRepository.save(product);
+    }
+}
